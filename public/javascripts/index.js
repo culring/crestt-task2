6c8246f14111a60f5954cc02b50c03e81f88f9ad
Rafał Wójcik <rafalwojcik@e-point.pl>
2020-07-23 08:34:30 +0200

    Interactive list

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,12 +1,50 @@
 $(document).ready(function() {
-    $(".children").click(function(){
-        $(".children").show()
-        if(shown) {
-            $(".children").hide()
+    const listItemsSelector = $("li:not(.empty)");
+
+    listItemsSelector.click(function(event) {
+        const childrenListSelector = $("ul", this).first();
+        if(childrenListSelector.length > 0) {
+            if($(this).hasClass("opened")) {
+                $(this).removeClass("opened");
+                $(this).addClass("closed");
+            }
+            else {
+                $(this).removeClass("closed");
+                $(this).addClass("opened");
+            }
+            childrenListSelector.slideToggle();
         }
-        else {
-            $(".children").show()
+
+        updateBulletForListItem(this,
+            "/assets/images/bullet_opened.png",
+            "/assets/images/bullet_closed.png");
+
+        event.stopPropagation();
+    });
+
+    listItemsSelector.hover(function(event) {
+        updateBulletForListItem(this,
+            "/assets/images/bullet_opened_hovered.png",
+            "/assets/images/bullet_closed_hovered.png")
+        event.stopPropagation();
+    }, function(event) {
+        updateBulletForListItem(this,
+            "/assets/images/bullet_opened.png",
+            "/assets/images/bullet_closed.png");
+        event.stopPropagation();
+    });
+
+    function updateBulletForListItem(listItem, openedImgSrc, closedImgSrc) {
+        const spanSelector = $("span", listItem).first();
+        const span = spanSelector.get(0);
+        const img = document.createElement('img');
+        if($(listItem).hasClass("opened")) {
+            img.src = openedImgSrc;
         }
-        shown = !shown;
-    })
+        else if($(listItem).hasClass("closed")) {
+            img.src = closedImgSrc;
+        }
+        span.textContent = "";
+        span.appendChild(img);
+    }
 });
