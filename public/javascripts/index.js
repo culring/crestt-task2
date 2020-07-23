e6de5f39591e47ee8a0c19ae8174dee38132f32d
Rafał Wójcik <rafalwojcik@e-point.pl>
2020-07-23 09:35:39 +0200

    Visual improvements

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,50 +1,64 @@
 $(document).ready(function() {
-    const listItemsSelector = $("li:not(.empty)");
+    addEventsForTogglingListItems();
+    addEventsForHoveringOverListItems();
+});
 
-    listItemsSelector.click(function(event) {
-        const childrenListSelector = $("ul", this).first();
+function addEventsForTogglingListItems() {
+    const listItemRowSelector = $("li:not(.empty)>span.node_description");
+    listItemRowSelector.click(function(event) {
+        const parentListItem = this.parentElement;
+        const childrenListSelector = $("ul", parentListItem).first();
         if(childrenListSelector.length > 0) {
-            if($(this).hasClass("opened")) {
-                $(this).removeClass("opened");
-                $(this).addClass("closed");
+            if($(parentListItem).hasClass("opened")) {
+                $(parentListItem).removeClass("opened");
+                $(parentListItem).addClass("closed");
             }
             else {
-                $(this).removeClass("closed");
-                $(this).addClass("opened");
+                $(parentListItem).removeClass("closed");
+                $(parentListItem).addClass("opened");
             }
             childrenListSelector.slideToggle();
         }
 
-        updateBulletForListItem(this,
-            "/assets/images/bullet_opened.png",
-            "/assets/images/bullet_closed.png");
+        updateBulletForListItem(parentListItem,
+            "/assets/images/bullet_opened_hovered.png",
+            "/assets/images/bullet_closed_hovered.png");
 
         event.stopPropagation();
     });
+}
 
-    listItemsSelector.hover(function(event) {
-        updateBulletForListItem(this,
+function addEventsForHoveringOverListItems() {
+    const listItemRowSelector = $("li:not(.empty)>span.node_description,span.bullet");
+    listItemRowSelector.hover(function(event) {
+        const parentListItem = this.parentElement;
+        const nodeDescriptionSelector = $("span.node_description", parentListItem).first();
+        nodeDescriptionSelector.css("font-weight", "bold");
+        updateBulletForListItem(parentListItem,
             "/assets/images/bullet_opened_hovered.png",
             "/assets/images/bullet_closed_hovered.png")
         event.stopPropagation();
     }, function(event) {
-        updateBulletForListItem(this,
+        const parentListItem = this.parentElement;
+        const nodeDescriptionSelector = $("span.node_description", parentListItem).first();
+        nodeDescriptionSelector.css("font-weight", "normal");
+        updateBulletForListItem(parentListItem,
             "/assets/images/bullet_opened.png",
             "/assets/images/bullet_closed.png");
         event.stopPropagation();
     });
+}
 
-    function updateBulletForListItem(listItem, openedImgSrc, closedImgSrc) {
-        const spanSelector = $("span", listItem).first();
-        const span = spanSelector.get(0);
-        const img = document.createElement('img');
-        if($(listItem).hasClass("opened")) {
-            img.src = openedImgSrc;
-        }
-        else if($(listItem).hasClass("closed")) {
-            img.src = closedImgSrc;
-        }
-        span.textContent = "";
-        span.appendChild(img);
+function updateBulletForListItem(listItem, openedImgSrc, closedImgSrc) {
+    const spanSelector = $("span.bullet", listItem).first();
+    const span = spanSelector.get(0);
+    const img = document.createElement('img');
+    if($(listItem).hasClass("opened")) {
+        img.src = openedImgSrc;
     }
-});
+    else if($(listItem).hasClass("closed")) {
+        img.src = closedImgSrc;
+    }
+    span.textContent = "";
+    span.appendChild(img);
+}
